f4f58e78bac3408ee0a19396e5c473fc8449ea54
ArtsemDev <thommy.python.dev@gmail.com>
2023-04-17 21:29:26 +0300

    lesson3

---
+++ lesson3.py
@@ -0,0 +1,145 @@
+# name = 'Alex'
+# age = 35
+
+
+# # def print(*args, sep='---', end=' finish! '):
+# # 	import builtins
+# # 	builtins.print(*args, end=end, sep=sep)
+
+# print(name, age)
+# print(name, age)
+
+
+# age = input('Enter age: ')
+# print(type(age))
+
+# a = 456
+# a += 2
+
+
+# text = 'hello world'
+
+# text = text.upper()
+# print(text)
+
+# numbers = [1, 2, 3, 4, 5]
+# numbers.append(6)
+# print(numbers)
+
+# print(True + True)
+# a = '-.456'
+# print(float(a))
+
+# a = 'inf'
+# b = 'nan'
+# print(float(a))
+# print(float(b))
+# print(float('4e34'))
+
+
+# a = '1_000_000'
+# print(int(a))
+
+
+# print(4 / 2)
+# print(4 // 3.0)
+# print(5 % 2)
+
+
+# text = 'hello\\ "pyt\'hon" world'
+# print(text)
+# print('hello\apython')
+# hello
+#      python
+# print('hello\tworld')
+
+# name = 'Alex'
+# surname = 'Popov'
+# fullname = name + surname
+# print(fullname*2)
+
+# print(abs(-789456))
+
+# print(len('hello world'))
+# print(r'C:\Users\Documents\\\\\\\n')
+
+# name = 'Alex'
+# age = 35
+# print('Hello ' + name + ' your age ' + str(35))
+# print('Hello %(first_name)s your age %(user_age)d' % {'user_age': age, 'first_name': name})
+# print('Hello {} your age {}'.format(name, age))
+# print(f'Hello {name} your age {age}')
+
+# print(f'{35} \N{fire}')
+# print('🔥')
+# text = 'hello---world---python---pycharm'
+# print(text.split('---', 2))
+
+# words = ('word', 'python', 'pycharm')
+# text = ''.join(words)
+# print(text)
+
+# text = 'hello'
+# print('-'.join(text))
+# text = 'hello python world python pycharm'
+# print(text.partition('java'))
+# print(text.rpartition('java'))
+# print(text.find('python', 10, 25))
+
+
+# text = 'hello <b>world</b> python'
+# text = text.replace('<b>', '').replace('</b>', '')
+# print(text)
+# text = '123456789'
+# print(text.isdigit())
+# print(text.isdecimal())
+# print('heLLo PyThON ß'.swapcase())
+# print('hello\tpython\tworld\tpycharm'.expandtabs(12))
+
+# text = '.-.-.-.-.,=hello-.,-.=world-=-=-=,.,.,.'
+# print(text.lstrip(',.-='))
+# print(text.rstrip(',.-='))
+# print(text.strip(',.-='))
+
+# text = 'hello'
+# print(text.ljust(10, '-'))
+# print(text.rjust(10, '-'))
+# print(text.zfill(10))
+
+# print(bin(14)[2:].zfill(16))
+# text = 'hello'
+# print(text.ljust(10, '-').rjust(15, '-'))
+
+# text = 'питон'
+# print(len(text.encode()))
+# print(2.0 == 2)
+# print('Hell' not in 'world hello python')
+
+
+# a = 4
+# b = 2 * 2
+# print(a is b)
+# text = 'hello'
+# print(not text)
+
+
+# a = 13
+# b = 11
+# print(bin(a))
+# print(bin(b))
+# print(a & b)
+# print(a | b)
+# print(a ^ b)
+# print(~-10)
+
+# print(bin(15 << 2))
+# print(bin(13))
+# print(bin(13 >> 2))
+# number = int(input())
+# n = 128
+
+# print((n & (n - 1) == 0) and n != 0)
+# print(bin(8))
+# print(bin(7))
+# print(128 & 127)
+# print(bin(32).count('0') - 1)
